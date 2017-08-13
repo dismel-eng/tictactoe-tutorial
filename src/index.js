bbcaf62582323d31e1dc2824728d4d38c99468ae
dismel-eng <eng.dismel@gmail.com>
2017-08-13 09:20:05 -0700

    adding funtionalities

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-);
+import './css/style.css'
+import './js/app'
